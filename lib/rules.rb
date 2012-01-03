1a24ebd96510636e5038b89f3f36ef9e3de66ef3
thierry <thierry.henrio@gmail.com>
2012-01-03 18:50:54 +0100

    extract

--- lib/rules.rb
+++ lib/rules.rb
@@ -10,20 +10,21 @@ module Fame
 
     def score(object, score={})
       op, points, *tail = expression
-      if attribute.to_sym == :repository
+      if repository?
         repository = OpenStruct.new(object)
         statement = (["score[:#{repository.name}] #{op} #{points}"] + tail).join(" ")
         eval(statement)
       else
         count = object
-        statement = "self[:#{attribute}] #{op} #{count} * #{points}"
-        score.instance_eval(statement)
+        statement = "score[:#{attribute}] #{op} #{count} * #{points}"
+        eval(statement)
       end
       score
     end
 
-    def pluralize(attribute)
-      "#{attribute}s"
+    private
+    def repository?
+      attribute.to_sym == :repository
     end
   end
 end
